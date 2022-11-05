876f058f0a95eb241f227e73194069a513630c38
InfiniteTachyon <t60.david@gmail.com>
2022-11-05 10:11:27 -0600

    added instructions.php

---
+++ instructions.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Instruction</title>
+    <link rel="stylesheet" type="text/css" href="inc/style.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="keywords" content="game">
+    <meta name="description" content="Game">
+    <meta name="author" content="SEC Team">
+</head>
+<body>
+    <header id="main-header">
+        <div class="container">
+            <h1 class="header-text">Instructions</h1>
+            <ul id="navbar">
+                <li><a href="index.php">Back To Home</a></li>
+            </ul>
+        </div>
+    </header>
+    <div class="container">
+        <div>
+            <h3> Single Player </h3> <br>
+            <ul>
+                <li>Click Roll</li>
+            </ul>
+        </div>
+    </div>
+    <script src="test.js"></script>
+</body>
