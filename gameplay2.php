328b215d720bd33a68086ffc11ffa232ec539803
shanellelck <shanellelck@outlook.com>
2022-11-05 12:29:49 -0600

    trying to style gameplayer2

--- gameplay2.php
+++ gameplay2.php
@@ -38,7 +38,7 @@ for ($turnNum=0; $turnNum<NUM_TURNS; $turnNum++) {
 <html lang="en">
 <head>
     <title>SEC Game</title>
-    <link rel="stylesheet" type="text/css" href="inc/style.css">
+    <link rel="stylesheet" type="text/css" href="style.css">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="keywords" content="game">
     <meta name="description" content="Game">
